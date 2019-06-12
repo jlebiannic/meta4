4ba9adf178b2f387ed1b4914478cba9fd81828a6
Johann LE BIANNIC <jlebiannic@sodifrance.fr>
2019-06-12 10:12:36 +0200

    fixed untracked files

--- src/parser/model/Salarie.java
+++ src/parser/model/Salarie.java
@@ -1,29 +1,59 @@
 package parser.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import java.util.UUID;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+import lombok.Getter;
+import lombok.Setter;
+
 public class Salarie extends ObjectWithUtParent {
     //	Salarie matricule="00009J" nomFamille="GONVER" prenom="VERONIQUE" mail="veronique.gonver@toto.com"
+    @Getter
+    @Setter
+    private String id;
+    @Getter
+    @Setter
     private String matricule;
+    @Getter
+    @Setter
     private String nom;
+    @Getter
+    @Setter
     private String prenom;
+    @Getter
+    @Setter
     private String mail;
+    @Getter
+    @Setter
     private String typeContrat;
+    @Getter
+    @Setter
     private Double horaireContractuel;
+    @Getter
+    @Setter
     private Double forfaitJourIndividuel;
+    @Getter
+    @Setter
     private String typeHoraire;
+    @Getter
+    @Setter
     private String typeVariabilite;
+    @Getter
+    @Setter
     private String fonctionCode;
+    @Getter
+    @Setter
     private String fonctionLibelle;
+    @Getter
+    @Setter
     private String type;
 
-    public Salarie(String matricule, String nomFamille, String prenom, String mail) {
+    public Salarie() {
         super();
+        this.id = UUID.randomUUID().toString();
+    }
+
+    public Salarie(String matricule, String nomFamille, String prenom, String mail) {
+        this();
         this.matricule = matricule;
         this.nom = nomFamille;
         this.prenom = prenom;
--- src/parser/model/UniteTravail.java
+++ src/parser/model/UniteTravail.java
@@ -1,23 +1,41 @@
 package parser.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import java.util.UUID;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+import lombok.Getter;
+import lombok.Setter;
+
 public class UniteTravail extends ObjectWithUtParent {
     //	<UniteTravail codeUT="0020" datedebut="1999-09-01" dateFin="4000-01-01" libelle="XX - Direction" codeSociete="001" codeEtablissement="S7901">
+    @Getter
+    @Setter
+    private String id;
+    @Getter
+    @Setter
 	private String codeUT;
+    @Getter
+    @Setter
 	private String dateDebut;
+    @Getter
+    @Setter
 	private String dateFin;
+    @Getter
+    @Setter
 	private String libelle;
+    @Getter
+    @Setter
 	private String codeSociete;
+    @Getter
+    @Setter
 	private String codeEtablissement;
 
+    public UniteTravail() {
+        super();
+        this.id = UUID.randomUUID().toString();
+    }
 
     public UniteTravail(String codeUT, String libelle, String codeSociete, String codeEtablissement) {
+        this();
         this.codeUT = codeUT;
         this.libelle = libelle;
         this.codeSociete = codeSociete;
